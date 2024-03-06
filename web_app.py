19adff1483cf2f63b4e8abbd8c47a78142f627e0
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2024-03-07 01:41:24 +0700

    simplify calling getMessage
    change hello_world() to index(), wont give effect to anything

--- web_app.py
+++ web_app.py
@@ -11,18 +11,24 @@ bytecode = MessageStorage.bytecode
 from web3 import Web3
 w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
 
-#contract address created after deploying contract
-msgContract = w3.eth.contract(address='0x6945a73F33bB02526ed89cA5D95BA4312f0EE490', abi=abi)
-currentMessage = msgContract.functions.getMessage().call()
 
-print(currentMessage)
+#function to get message from contract
+def getMessageFromContract(tnx_address):
+    msgContract = w3.eth.contract(address=tnx_address, abi=abi)
+    currentMessage = msgContract.functions.getMessage().call()
+    print(currentMessage)
+    return currentMessage
+
 
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-
-    debugStr = "<p>Abi: " + str(abi) + "<br>Bytecode: " + str(bytecode) + "</p>"
-    return debugStr
+def index():
+    #debugStr = "<p>Abi: " + str(abi) + "<br>Bytecode: " + str(bytecode) + "</p>"
+    
+    #contract address created after deploying contract
+    message_text = getMessageFromContract('0x6945a73F33bB02526ed89cA5D95BA4312f0EE490')
+    displayStr = "<p>" + message_text + "</p>"
+    return displayStr
