0192d1cdcbca5b86a0a9a43c45720e12abeb6c94
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2024-03-07 01:26:53 +0700

    calling function inside contract
    call getMessage() function from deployed contract address

--- web_app.py
+++ web_app.py
@@ -7,8 +7,18 @@ from brownie.project.BrownieDirProject import *
 abi = MessageStorage.abi
 bytecode = MessageStorage.bytecode
 
-from flask import Flask
 
+from web3 import Web3
+w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
+
+#contract address created after deploying contract
+msgContract = w3.eth.contract(address='0x6945a73F33bB02526ed89cA5D95BA4312f0EE490', abi=abi)
+currentMessage = msgContract.functions.getMessage().call()
+
+print(currentMessage)
+
+
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
