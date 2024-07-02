c7000e252bd2d58b3400d5ca04d42c5114d7abdc
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2024-07-02 15:04:56 +0700

    restore getMessageHashOld

--- web_app.py
+++ web_app.py
@@ -72,8 +72,7 @@ def readMessageFromHash(tx_hash, sndr_addr, rcvr_addr):
                     if to_addr == rcvr_addr:
                         restricted_text = pack_msg['value']
                     else:
-                        restricted_text = "ERROR : Wrong Receiver's Address"
-                                    
+                        restricted_text = "ERROR : Wrong Receiver's Address"                                    
                 else:
                     restricted_text = "ERROR : Wrong Sender's Address"
                                            
@@ -118,6 +117,47 @@ def grantAccessToAccount(ctx, owner, acc, allow):
         res_message = 'Access Disabled'
 
     return res_message
+
+def setMessageCtxOld(ctx_addr, sndr_addr, sndr_pk, value):
+    contract = w3.eth.contract(address=ctx_addr, abi=res_abi)
+    tx = contract.functions.writeText(value).build_transaction({
+        'from' : sndr_addr,
+        'nonce' : w3.eth.get_transaction_count(sndr_addr),
+        'gasPrice': 200000
+    })
+
+    signed_tx = w3.eth.account.sign_transaction(tx, sndr_pk)
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+    print(tx_receipt)
+    
+    now = datetime.now() # current date and time
+    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")    
+    log_row = date_time + ";" + tx_receipt['transactionHash'].hex()  + ";" +  str(tx_receipt['blockNumber'])  + ";" +  str(tx_receipt['cumulativeGasUsed']) + ";" +  str(tx_receipt['gasUsed'])  + ";" +  tx_receipt['from']  + ";" +  tx_receipt['to']  + ";" +  str(tx_receipt['effectiveGasPrice']) + ";\n"
+    writeGanacheLog(file_name,log_row)
+    
+    return tx_receipt['transactionHash'].hex()
+
+
+def readMessageFromHashOld(tx_hash, sndr_addr):    
+    tx = w3.eth.get_transaction(tx_hash)
+    ctx_address = tx['to']    
+    contract = w3.eth.contract(address=ctx_address, abi=res_abi)
+
+    tx_receipt = w3.eth.get_transaction_receipt(tx_hash)
+    restricted_text = '-'
+               
+    if tx_receipt.status == 1:
+    # Proses log acara dari transaksi tersebut
+        for log in tx_receipt.logs:
+            # Periksa apakah log acara terkait dengan event TextChanged
+            if log.address == ctx_address:
+                # Dekode data dari log acara
+                decoded_log = contract.events.TextChanged().process_log(log)
+                restricted_text = decoded_log['args']['newText']                                
+    print(restricted_text)
+    return restricted_text
 
 #1-#36 blockchain (web3)
 
@@ -221,4 +261,44 @@ def grantAccess():
 
     return jsonify(
         message_text = res_message 
+    )
+    
+@app.route("/set_message_old", methods=['POST'])
+def setMessageOld():
+    res_message = '-'
+    # print(request.method)
+    if request.method == 'POST':
+        sender_address = request.form['sender_address']
+        sender_pk = request.form['sender_pk']
+        address = request.form['address']
+        value = request.form['value']
+        res_message = setMessageCtxOld(address, sender_address, sender_pk, value)
+        print(res_message)
+    else:
+        res_message = 'error'
+
+    return jsonify(
+        message_text = res_message 
+    )
+
+
+
+@app.route("/get_message_hash_old", methods=['POST'])
+def getMessageHashOld():
+    message_text = ''
+    status = 'empty'    
+    if request.method == 'POST':     
+        tx_hash = request.form['tx_hash']
+        sender_address = request.form['sender_address']                
+        try:                        
+            message_text = readMessageFromHashOld(tx_hash, sender_address) #contract address created after deploying contract            
+        except:
+            message_text = 'error'
+        # return f"<p> Getting Message from Address : {escape(address)} <br>The Message : {escape(message_text)} </p>"
+   
+    else:
+        message_text = 'wrong method'
+
+    return jsonify(
+        message_text = message_text
     )
