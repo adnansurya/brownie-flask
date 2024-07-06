7a92f9779d27fa2a5c7221c38419f7b388fef7ae
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2024-07-07 01:24:57 +0700

    add debug error function

--- web_app.py
+++ web_app.py
@@ -17,77 +17,109 @@ w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
 from datetime import datetime
 
 file_name = datetime.now().strftime("%m%d%Y_%H%M%S")
+debug_filename = "debug_" + file_name
 print("nama file: " + file_name)
 
 
-def accountIsIdentified(acc_addr):    
-    isIdentified = False
-    list_acc = w3.eth.accounts
-    for acc in list_acc:
-        if acc == acc_addr:
-            isIdentified = True
-            break
-    
-    return isIdentified
+
 
 def writeGanacheLog(fileName, row):    
     # open the file in the write mode
     with open('output_log/'+ fileName + '.csv', 'a') as f:       
         f.write(row)
+        
 
+def writeDebugLog(fileName, func, msg, desc): 
+    now = datetime.now() # current date and time
+    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")  
+    debug_row = str(date_time) + ';'+ func +';'+ msg +';'+ str(desc) + ';\n'    
+    # open the file in the write mode
+    with open('debug_log/'+ fileName + '.csv', 'a') as f:       
+        f.write(debug_row)
+
+
+def accountIsIdentified(acc_addr):   
+    
+    isIdentified = False
+    try: 
+        list_acc = w3.eth.accounts
+        for acc in list_acc:
+            if acc == acc_addr:
+                isIdentified = True
+                break
+    except Exception as e: 
+        print(f"An error occurred: {e}")
+        writeDebugLog(debug_filename, 'accountIsIdentified', '-' , e)        
+    
+    return isIdentified
 
 def setMessageCtx(ctx_addr, sndr_addr, sndr_pk, value):
-    to_addr = json.loads(value)['to_address']
-    contract = w3.eth.contract(address=ctx_addr, abi=res_abi)
-    tx = contract.functions.writeText(value).build_transaction({
-        'from' : sndr_addr,
-        'nonce' : w3.eth.get_transaction_count(sndr_addr),
-        'gasPrice': 200000
-    })
-
-    signed_tx = w3.eth.account.sign_transaction(tx, sndr_pk)
-    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-
-    print(tx_receipt)
+    hashResult = ''
     
-    now = datetime.now() # current date and time
-    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")    
-    log_row = date_time + ";" + tx_receipt['transactionHash'].hex()  + ";" +  str(tx_receipt['blockNumber'])  + ";" +  str(tx_receipt['cumulativeGasUsed']) + ";" +  str(tx_receipt['gasUsed'])  + ";" +  tx_receipt['from']  + ";" +  tx_receipt['to']  + ";" +  str(tx_receipt['effectiveGasPrice']) + ";" + to_addr + ";\n"
-    writeGanacheLog(file_name,log_row)
+    try:    
+        to_addr = json.loads(value)['to_address']
+        contract = w3.eth.contract(address=ctx_addr, abi=res_abi)
+        tx = contract.functions.writeText(value).build_transaction({
+            'from' : sndr_addr,
+            'nonce' : w3.eth.get_transaction_count(sndr_addr),
+            'gasPrice': 200000
+        })
+        signed_tx = w3.eth.account.sign_transaction(tx, sndr_pk)
+        tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+        print(tx_receipt)
     
-    return tx_receipt['transactionHash'].hex()
+        now = datetime.now() # current date and time
+        date_time = now.strftime("%m/%d/%Y, %H:%M:%S")    
+        log_row = date_time + ";" + tx_receipt['transactionHash'].hex()  + ";" +  str(tx_receipt['blockNumber'])  + ";" +  str(tx_receipt['cumulativeGasUsed']) + ";" +  str(tx_receipt['gasUsed'])  + ";" +  tx_receipt['from']  + ";" +  tx_receipt['to']  + ";" +  str(tx_receipt['effectiveGasPrice']) + ";" + to_addr + ";\n"
+        writeGanacheLog(file_name,log_row)
+        hashResult = tx_receipt['transactionHash'].hex()
+                    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        writeDebugLog(debug_filename, 'setMessageCtx', hashResult , e)        
+
+    return hashResult
+   
 
 
-def readMessageFromHash(tx_hash, sndr_addr, rcvr_addr):       
-    tx = w3.eth.get_transaction(tx_hash)
-    ctx_address = tx['to']    
-    contract = w3.eth.contract(address=ctx_address, abi=res_abi)
+def readMessageFromHash(tx_hash, sndr_addr, rcvr_addr): 
+    restricted_text = ''
+    try :      
+        tx = w3.eth.get_transaction(tx_hash)
+        ctx_address = tx['to']    
+        contract = w3.eth.contract(address=ctx_address, abi=res_abi)
 
-    tx_receipt = w3.eth.get_transaction_receipt(tx_hash)
-    restricted_text = '-'
-               
-    if tx_receipt.status == 1:
-    # Proses log acara dari transaksi tersebut
-        for log in tx_receipt.logs:
-            # Periksa apakah log acara terkait dengan event TextChanged
-            if log.address == ctx_address:                
-                # Dekode data dari log acara
-                decoded_log = contract.events.TextChanged().process_log(log)                
-                decoded_text = decoded_log['args']['newText']    
-                changer = decoded_log['args']['changer']
-                if changer == sndr_addr:
-                    pack_msg = json.loads(decoded_text)
-                    to_addr = pack_msg['to_address']
-                    print(to_addr)
-                    if to_addr == rcvr_addr:
-                        restricted_text = pack_msg['value']
+        tx_receipt = w3.eth.get_transaction_receipt(tx_hash)
+        restricted_text = '-'
+                
+        if tx_receipt.status == 1:
+        # Proses log acara dari transaksi tersebut
+            for log in tx_receipt.logs:
+                # Periksa apakah log acara terkait dengan event TextChanged
+                if log.address == ctx_address:                
+                    # Dekode data dari log acara
+                    decoded_log = contract.events.TextChanged().process_log(log)                
+                    decoded_text = decoded_log['args']['newText']    
+                    changer = decoded_log['args']['changer']
+                    if changer == sndr_addr:
+                        pack_msg = json.loads(decoded_text)
+                        to_addr = pack_msg['to_address']
+                        print(to_addr)
+                        if to_addr == rcvr_addr:
+                            restricted_text = pack_msg['value']
+                        else:
+                            restricted_text = "Wrong Receiver's Address" 
+                            writeDebugLog(debug_filename, 'readMessageFromHash', restricted_text , '-')                                   
                     else:
-                        restricted_text = "ERROR : Wrong Receiver's Address"                                    
-                else:
-                    restricted_text = "ERROR : Wrong Sender's Address"
-                                           
-    print(restricted_text)
+                        restricted_text = "Wrong Sender's Address"
+                        writeDebugLog(debug_filename, 'readMessageFromHash', restricted_text , '-')     
+                                            
+        print(restricted_text)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        writeDebugLog(debug_filename, 'readMessageFromHash', restricted_text , e)  
+        
     return restricted_text
 
 def readMessageFromContract(ctx, sndr_addr):    
@@ -179,6 +211,10 @@ import json
 log_header = 'timestamp ;txHash ;blockNumber ;cumulativeGasUsed ;gasUsed ;from ;to ;effectiveGasPrice; toAddr; \n' 
 with open('output_log/'+ file_name+ '.csv', 'w') as f:
     f.write(log_header)
+    
+debug_header = 'timestamp ;function; message; description;\n' 
+with open('debug_log/'+ debug_filename+ '.csv', 'w') as f:
+    f.write(debug_header)
 
 app = Flask(__name__)
 
@@ -210,8 +246,11 @@ def setMessage():
             print(res_message)
         else:
             res_message = 'Unidentified Account'
+            writeDebugLog(debug_filename, 'set_message', res_message, str(request.form))            
     else:
-        res_message = 'error'
+        res_message = 'Wrong Request Method'
+        writeDebugLog(debug_filename, 'set_message', res_message, str(request.form))
+        
 
     return jsonify(
         message_text = res_message 
@@ -230,13 +269,16 @@ def getMessageHash():
         if accountIsIdentified(sender_address):                       
             try:                        
                 message_text = readMessageFromHash(tx_hash, sender_address, receiver_address) #contract address created after deploying contract            
-            except:
-                message_text = 'error'
+            except Exception as e:
+                message_text = 'Error Reading Message From Hash'
+                writeDebugLog(debug_filename, 'get_message_hash', message_text, e)
         # return f"<p> Getting Message from Address : {escape(address)} <br>The Message : {escape(message_text)} </p>"
         else:
             message_text = 'Unidentified Account'
+            writeDebugLog(debug_filename, 'get_message_hash', message_text, str(request.form))
     else:
-        message_text = 'wrong method'
+        message_text = 'Wrong Request Method'
+        writeDebugLog(debug_filename, 'get_message_hash', message_text, str(request.form))
 
     return jsonify(
         message_text = message_text
@@ -321,12 +363,5 @@ def getMessageHashOld():
         message_text = message_text
     )
     
-@app.route("/tes_post", methods=['POST'])
-def testPost():
-    if request.method == 'POST': 
-        sender_address = request.form['sender_address']
-        # value = request.form['value']
-        return(sender_address )
-    else:
-        return 'ERROR'
+
     
